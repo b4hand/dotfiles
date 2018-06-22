5edada51b77bbb742d5497f1710e5fcb49201c67
Brandon Forehand <b4hand@users.sf.net>
2018-06-22 08:40:56 -0700

    Add basic install script.

---
+++ mac/init.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew install git
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install firefox
+brew cask install iterm2
+brew install vagrant
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install dropbox
+brew cask install java
+brew install coreutils
+brew install wget
+brew install bash-completion
+brew install lesspipe
+brew install gnu-getopt --with-default-names
+brew install bash
+brew install findutils --with-default-names
+brew install iftop
+brew install jq
+brew install docker
+brew install docker-machine
+brew install docker-compose
+brew install pyenv
+brew install hub
+brew install watch
+brew install rbenv
+# defaults -currentHost write -globalDomain AppleFontSmoothing -int 1
+# brew install gmp mpfr oniguruma qt5 # missing deps
+# brew install gpg
+# brew install qcachegrind
+# brew install teleconsole
+# brew install rake-completion
+# brew cask install bettertouchtool
+# brew search vagrant
+# brew cask install menumeters
+# brew cask install flash-player
+# brew cask install chromium
+brew tap b4hand/bsdmainutils
+brew install bsdmainutils
+brew cask install keepingyouawake
+
+# brew install markdown
+# brew install npm
+# brew install ruby
+# brew cask install chefdk
+# brew cask install xquartz
+# brew cask install vlc
+# brew cask install skype
+# brew cask install handbrake
+# brew cask install videomonkey
+
+# pip install matplotlib
+
+# brew install icu4c
+# brew link icu4c
+# brew link icu4c  --force
+# brew install maven
+
+# brew install pstree
+# brew install sloccount
+
+# brew tap caskroom/versions
+# brew cask install firefox-esr
+# brew cask install google-chrome
+# brew install redis
+# brew install libevent
+# brew install csshx
+
+# brew install unrar
+
+# brew install expect
